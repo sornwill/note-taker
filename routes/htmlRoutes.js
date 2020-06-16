2f8e05da457e0de168308c06ff3f4899786487b9
Soul <sornwilliam@gmail.com>
2020-06-16 13:42:15 -0500

    added start script in package. Changed notes directory in index.html. updated server.js/htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,12 +4,12 @@ const path = require("path");
 //Routing
 
 module.exports = function(app){
-    app.get("/index.html", function(req, res) {
-        res.sendFile(path.join(_dirname, "../public/index.html"));
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
     app.get("/notes.html", function(req,res) {
-        res.sendFile(path.join(_dirname, "../public/notes.html"));
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
     console.log("html Routes Confirmed")
